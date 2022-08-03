4dd18519d3e726f95cc92ee7df83326aa0c9bbbf
Judekeyser <justin.dekeyser@gmail.com>
2022-08-03 11:27:29 +0200

    Smaller core API

--- python_versioning.py
+++ python_versioning.py
@@ -1,11 +1,10 @@
 import re
-import git
 
 
 class Version:
-    def __init__(self, first, second, third):
+    def __init__(self, first, second, third=0):
         self.expand = (first, second, third)
-        self.commit = None
+        self.cursor = None
 
     def __lt__(self, other):
         for s, o in zip(self.expand, other.expand):
@@ -17,48 +16,60 @@ class Version:
         return Version(*(a+b for a,b in zip(self.expand, (0,0,1))))
 
 
-class GitVersionTag(Version):
-    __PATTERN = re.compile(r'v(\d+)\.(\d+)\.(\d+)([^\d].)*')
+class GitTag(Version):
+    __PATTERN = re.compile(r'(\d+)\.(\d+)\.(\d+)([^\d].)*')
 
-    def __init__(self, git_tag):
-        m = GitVersionTag.__PATTERN.match(str(git_tag))
+    def __init__(self, version):
+        m = GitTag.__PATTERN.match(version['name'])
         Version.__init__(self, *(int(m.group(_)) for _ in range(1,4)))
-        self.commit = git_tag.commit
+        self.cursor = version['cursor']
 
 
-def get_next_version(version_file_path):
+def get_next_version(collection=[], lower_bound='0.0', cursor=None):
     """
-    Get the next version, based on GIT repository state, content of version file and release type.
+    Get the next version based on the provided collection.
+    The next version is guaranteed to be strictly higher than the provided lower_bound,
+    only if the highest point in the collection corresponds to a different cursor than the provided one.
     
     Parameters
     ----------
-    version_file_path : str
-      The path to the file containing the reference {major}.{minor}
+    collection
+        The collection of versions to consider
+    lower_bound
+        The lower bound we guarantee to be strictly higher
+    cursor
+        The cursor we are in. Versions might be associated to cursors.
     
     Returns
     -------
-      next_version : str
-        The next version, as a string of the form {major}.{minor}.{patch}
+      next_version
+        The next version, as a triplet
     """
-    # Get information from Git Repository
-    repo = git.Repo(search_parent_directories=True)
-    highest_git_tag = max((GitVersionTag(_) for _ in repo.tags if str(_).startswith('v')), default=Version(0,0,0))
-
-    # Get information from auxiliary file
-    with open(version_file_path, 'r') as f:
-        line = f.readline().strip()
-    m = re.compile(r'(\d+)\.(\d+)').match(line)
-    next_version = Version(*(int(m.group(_)) for _ in range(1,3)), 0)
+    next_version = Version(*(int(_) for _ in lower_bound.split('.')))
+    highest_git_tag = max((GitTag(_) for _ in collection), default=next_version)
 
     # Combine and retrieve if we are higher than git version
-    if next_version < highest_git_tag:
+    if next_version < highest_git_tag or not highest_git_tag < next_version:
         next_version = highest_git_tag
-    if repo.head.commit != next_version.commit:
-        next_version = next_version.increment()
+        if cursor != next_version.cursor:
+            next_version = next_version.increment()
     
-    return '.'.join((str(i) for i in next_version.expand))
+    return next_version.expand
 
 
 if __name__ == '__main__':
     import sys
-    print(get_next_version(sys.argv[1]))
+    import git
+    # Information from git repository
+    repo = git.Repo(search_parent_directories=True)
+    # Information from auxiliary file
+    with open(sys.argv[1], 'r') as f:
+        lower_bound = f.readline().strip()
+    # Calling the function
+    next_version = get_next_version(
+        collection=({ 'name': str(_)[1:], 'cursor': _.commit }
+            for _ in repo.tags if str(_).startswith('v')),
+        lower_bound=lower_bound,
+        cursor=repo.head.commit
+    )
+    print('.'.join((str(i) for i in next_version)))
